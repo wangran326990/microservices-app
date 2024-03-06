7e0be5034822231d6facb1293e64f1c5973cc558
systems@amayasoftware.com <ran_wang@flutterint.com>
2024-03-06 12:01:49 -0500

    add config for the H2 database

--- easybank/accounts/src/main/java/com/easybank/accounts/controller/AccountsController.java
+++ easybank/accounts/src/main/java/com/easybank/accounts/controller/AccountsController.java
@@ -1,2 +1,13 @@
-package com.easybank.accounts.controller;public class AccountsController {
+package com.easybank.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+
+    @GetMapping("sayHello")
+    public String sayHello() {
+        return "Hello World";
+    }
 }
